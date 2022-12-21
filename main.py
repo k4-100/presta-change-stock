3c2c7d83f152e0e2770e02978381ea9092350ec0
k4-100 <k4-100@protonmail.com>
2022-12-21 11:57:44 +0100

    Początkowe połączenie

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
+import mysql.connector 
+from pprint import pprint
+
+def perform_connection(db_info):
+    '''
+
+    '''
+    cnx = mysql.connector.connect(
+        user=db_info['user'],
+        password=db_info['password'],
+        host=db_info['host'],
+        database=db_info['database']
+    )
+    cur = cnx.cursor()
+    cur.execute('SELECT * FROM ps_stock_available')
+    row = cur.fetchall()
+    pprint(row)
+    
+    cnx.close()
+
 
 def main():
-    print('werks')
+    db_info = {
+        "user": "root",
+        "password": "",
+        "host": "127.0.0.1",
+        "database": "prestashop_1_6"
+    }
+
+    perform_connection(db_info)
 
 
 if __name__ == '__main__':
