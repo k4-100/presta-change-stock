9fac90a8b27fdbfc22b1633ddcba01b8ed96167a
k4-100 <k4-100@protonmail.com>
2022-12-22 14:42:43 +0100

    dodano command line flagi i try except

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import mysql.connector 
 from pprint import pprint
+import sys
 
 queryFetch = '''
 SELECT ps_stock_available.quantity FROM ps_stock_available, ps_product
@@ -8,42 +9,38 @@ AND ps_product.reference = "food_2";
 '''
 
 
-queryChange = lambda new_stock : f'''
+queryChange = lambda new_stock, reference_code: f'''
 UPDATE ps_stock_available, ps_product
 SET ps_stock_available.quantity = {new_stock}
 WHERE ps_stock_available.id_product = ps_product.id_product
-AND ps_product.reference = "food_2";
+AND ps_product.reference = "{reference_code}";
 '''
-# UPDATE ps_stock_available, ps_product 
-# SET ps_stock_available.quantity = 9999 
-# WHERE ps_stock_available.id_product = ps_product.id_product 
-# AND ps_product.reference = "food_2;
 
 
-def fetch_table(db_info):
-    '''
-    fetchuje tabelke z bazy danych
+# def fetch_table(db_info):
+#     '''
+#     fetchuje tabelke z bazy danych
 
-    :param db_info: dane bazy danych w formie słownika
-    :return: całą tabelke wyciągniętą z pliku
-    '''
-    cnx = mysql.connector.connect(
-        user=db_info['user'],
-        password=db_info['password'],
-        host=db_info['host'],
-        database=db_info['database']
-    )
-    cur = cnx.cursor()
-    cur.execute(queryFetch)
-    table = cur.fetchall()
-    
-    cur.close()
-    cnx.close()
+#     :param db_info: dane bazy danych w formie słownika
+#     :return: całą tabelke wyciągniętą z pliku
+#     '''
+#     cnx = mysql.connector.connect(
+#         user=db_info['user'],
+#         password=db_info['password'],
+#         host=db_info['host'],
+#         database=db_info['database']
+#     )
+#     cur = cnx.cursor()
+#     cur.execute(queryFetch)
+#     table = cur.fetchall()
+#     
+#     cur.close()
+#     cnx.close()
 
-    return table
+#     return table
 
 
-def change_table(db_info, value):
+def change_table(db_info, reference_code, new_stock):
     '''
     podmienia wartość dla quantity w bazie danych
 
@@ -56,11 +53,11 @@ def change_table(db_info, value):
         database=db_info['database']
     )
     cur = cnx.cursor()
-    cur.execute(queryChange(value))
+    cur.execute(queryChange(new_stock, reference_code))
     cnx.commit()
+
     cur.close()
     cnx.close()
-
 
 
 
@@ -72,9 +69,10 @@ def main():
         "database": "prestashop_1_6"
     }
     
-    change_table(db_info, 4567)
-
-    # fetch_table(db_info)
+    try:
+        change_table(db_info, sys.argv[1], sys.argv[2])
+    except:
+        print("BŁĄD: poprawna składnia: main.py kod_produktu nowa_ilość")
 
 
 if __name__ == '__main__':
